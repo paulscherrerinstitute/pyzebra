44eebe5a74ba1ce2864bdf84d125fd6624030613
Oksana Zaharko <Oksana.Zaharko@psi.ch>
2020-03-18 14:48:34 +0100

    OZ added z4frgn

--- pyzebra/zebra.py
+++ pyzebra/zebra.py
@@ -1,5 +1,5 @@
 import h5py
-
+import  numpy as np
 
 def read_h5meta(filepath):
     """Read and parse content of a h5meta file.
@@ -63,3 +63,24 @@ def open_h5meta(filepath):
         data[file] = read_detector_data(file)
 
     return data
+
+def z4frgn(wave,ga,nu):
+    """CALCULATES DIFFRACTION VECTOR IN LAB SYSTEM FROM GA AND NU
+
+    Args:
+        WAVE,GA,NU
+
+    Returns:
+        Z4
+    """
+    sin = np.sin
+    cos = np.cos
+    pir = 180/np.pi
+    gar = ga/pir
+    nur = nu/pir
+    z4 = [0., 0., 0.]
+    z4[0]=( sin(gar)*cos(nur)    )/wave
+    z4[1]=( cos(gar)*cos(nur)-1. )/wave
+    z4[2]=( sin(nur)             )/wave
+
+    return z4
