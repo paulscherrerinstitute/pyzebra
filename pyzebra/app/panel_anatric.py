d9924ffc834a12978a2ce02286781da0c75d067c
Ivan Usov <ivan.usov@psi.ch>
2020-06-18 10:34:34 +0200

    Add RadioButtonGroup for operation mode

--- pyzebra/app/panel_anatric.py
+++ pyzebra/app/panel_anatric.py
@@ -1,17 +1,22 @@
 from bokeh.layouts import column
-from bokeh.models import Panel, TextInput, Button
+from bokeh.models import Panel, TextInput, Button, RadioButtonGroup
 
 import pyzebra
 
 
 def create():
     fileinput = TextInput()
-    process_button = Button(label="Process")
+
+    mode_radio_button_group = RadioButtonGroup(
+        labels=["Adaptive Peak Detection", "Adaptive Dynamic Mask Integration"], active=0
+    )
 
     def process_button_callback():
         pyzebra.anatric(fileinput.value)
 
+    process_button = Button(label="Process")
     process_button.on_click(process_button_callback)
-    
-    tab_layout = column(fileinput, process_button)
+
+    tab_layout = column(fileinput, mode_radio_button_group, process_button)
+
     return Panel(child=tab_layout, title="Anatric")
