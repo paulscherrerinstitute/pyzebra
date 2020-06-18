3e7915b9819d2567b96a2fce1c24a4c8a5445699
Ivan Usov <ivan.usov@psi.ch>
2020-06-18 15:01:54 +0200

    Add Spinner stubs for mode parameters

--- pyzebra/app/panel_anatric.py
+++ pyzebra/app/panel_anatric.py
@@ -1,15 +1,77 @@
-from bokeh.layouts import column
-from bokeh.models import Panel, TextInput, Button, RadioButtonGroup
+from bokeh.layouts import column, row
+from bokeh.models import Button, Panel, RadioButtonGroup, Spinner, TextInput
 
 import pyzebra
 
 
 def create():
-    fileinput = TextInput()
+    fileinput = TextInput(width=600)
+
+    # General parameters
+    # ---- logfile
+
+    # ---- FileList
+
+    # ---- crystal
+
+    # ---- DataFactory
+
+    # ---- BackgroundProcessor
+
+    # ---- DetectorEfficency
+
+    # ---- ReflectionPrinter
 
     mode_radio_button_group = RadioButtonGroup(
         labels=["Adaptive Peak Detection", "Adaptive Dynamic Mask Integration"], active=0
     )
+
+    # Adaptive Peak Detection (adaptivemaxcog)
+    # ---- threshold
+    threshold_spinner = Spinner(title="Threshold", value=None)
+
+    # ---- shell
+    shell_spinner = Spinner(title="Shell", value=None)
+
+    # ---- steepness
+    steepness_spinner = Spinner(title="Steepness", value=None)
+
+    # ---- duplicateDistance
+    duplicateDistance_spinner = Spinner(title="Duplicate Distance", value=None)
+
+    # ---- maxequal
+    maxequal_spinner = Spinner(title="Max Equal", value=None)
+
+    # ---- window
+    apd_window_spinner = Spinner(title="Window", value=None)
+
+    # Adaptive Dynamic Mask Integration (adaptivedynamic)
+    # ---- window
+    admi_window_spinner = Spinner(title="Window", value=None)
+
+    # ---- border
+    border_spinner = Spinner(title="Border", value=None)
+
+    # ---- minWindow
+    minWindow_spinner = Spinner(title="Min Window", value=None)
+
+    # ---- reflectionFile
+    reflectionFile_spinner = Spinner(title="Reflection File", value=None)
+
+    # ---- targetMonitor
+    targetMonitor_spinner = Spinner(title="Target Monitor", value=None)
+
+    # ---- smoothSize
+    smoothSize_spinner = Spinner(title="Smooth Size", value=None)
+
+    # ---- loop
+    loop_spinner = Spinner(title="Loop", value=None)
+
+    # ---- minPeakCount
+    minPeakCount_spinner = Spinner(title="Min Peak Count", value=None)
+
+    # ---- displacementCurve
+    displacementCurve_spinner = Spinner(title="Displacement Curve", value=None)
 
     def process_button_callback():
         pyzebra.anatric(fileinput.value)
@@ -17,6 +79,32 @@ def create():
     process_button = Button(label="Process")
     process_button.on_click(process_button_callback)
 
-    tab_layout = column(fileinput, mode_radio_button_group, process_button)
+    tab_layout = row(
+        column(fileinput, process_button),
+        column(
+            mode_radio_button_group,
+            row(
+                column(
+                    threshold_spinner,
+                    shell_spinner,
+                    steepness_spinner,
+                    duplicateDistance_spinner,
+                    maxequal_spinner,
+                    apd_window_spinner,
+                ),
+                column(
+                    admi_window_spinner,
+                    border_spinner,
+                    minWindow_spinner,
+                    reflectionFile_spinner,
+                    targetMonitor_spinner,
+                    smoothSize_spinner,
+                    loop_spinner,
+                    minPeakCount_spinner,
+                    displacementCurve_spinner,
+                ),
+            ),
+        ),
+    )
 
     return Panel(child=tab_layout, title="Anatric")
