4d571d2fd65830165a0e86d3cb3154b7cdfba6bb
Ivan Usov <ivan.usov@psi.ch>
2020-09-16 16:44:55 +0200

    Import new functions on pyzebra import

--- pyzebra/__init__.py
+++ pyzebra/__init__.py
@@ -1,3 +1,7 @@
 from pyzebra.anatric import *
 from pyzebra.h5 import *
 from pyzebra.xtal import *
+from pyzebra.load_1D import load_1D, parse_1D
+from pyzebra.ccl_findpeaks import ccl_findpeaks
+from pyzebra.fit2 import fitccl
+from pyzebra.comm_export import export_comm
