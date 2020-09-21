4c0b195b7a294ee3077f16e1c64ec5625e57af68
JakHolzer <53743814+JakHolzer@users.noreply.github.com>
2020-09-21 11:26:47 +0200

    Update comm_export.py
    Added the fit value/ num integrated value choice

--- pyzebra/comm_export.py
+++ pyzebra/comm_export.py
@@ -30,46 +30,46 @@ def export_comm(data, path, lorentz=False):
         padding = [4, 6, 10, 8]
 
     with open(str(path + extension), "w") as out_file:
-        for meas_num, meas in data["Measurements"].items():
-            print(meas_num)
-            if meas.get("fit") is None:
-                print("Measurement skipped - no fit value for:", meas_num)
-                continue
+        for keys in data["Measurements"]:
+            try:
+                meas_number_str = f"{keys[1:]:{align}{padding[0]}}"
+                h_str = f'{int(data["Measurements"][str(keys)]["h_index"]):{padding[1]}}'
+                k_str = f'{int(data["Measurements"][str(keys)]["k_index"]):{padding[1]}}'
+                l_str = f'{int(data["Measurements"][str(keys)]["l_index"]):{padding[1]}}'
+                if data["Measurements"][str(keys)]["fit"]["export_fit"] is True:
+                    area = float(data["Measurements"][str(keys)]["fit"]["g_amp"].value)
+                    sigma_str = f'{"{:8.2f}".format(float(data["Measurements"][str(keys)]["fit"]["g_amp"].stderr)):{align}{padding[2]}}'
+                else:
+                    area = float(data["Measurements"][str(keys)]["fit"]["int_area"].n)
+                    sigma_str = f'{"{:8.2f}".format(float(data["Measurements"][str(keys)]["fit"]["int_area"].s)):{align}{padding[2]}}'
 
-            meas_number_str = f"{meas_num[1:]:{align}{padding[0]}}"
-            h_str = f'{int(meas["h_index"]):{padding[1]}}'
-            k_str = f'{int(meas["k_index"]):{padding[1]}}'
-            l_str = f'{int(meas["l_index"]):{padding[1]}}'
-            if meas["fit"]["export_fit"] is True:
-                area = float(meas["fit"]["g_amp"][0]) + float(meas["fit"]["l_amp"][0])
-            else:
-                area = float(meas["fit"]["int_area"]) - float(meas["fit"]["int_background"][0])
+                if data["meta"]["zebra_mode"] == "bi":
+                    int_str = f'{"{:8.2f}".format(correction(area, lorentz, data["meta"]["zebra_mode"], data["Measurements"][str(keys)]["twotheta_angle"])):{align}{padding[2]}}'
+                    angle_str1 = f'{data["Measurements"][str(keys)]["twotheta_angle"]:{padding[3]}}'
+                    angle_str2 = f'{data["Measurements"][str(keys)]["omega_angle"]:{padding[3]}}'
+                    angle_str3 = f'{data["Measurements"][str(keys)]["chi_angle"]:{padding[3]}}'
+                    angle_str4 = f'{data["Measurements"][str(keys)]["phi_angle"]:{padding[3]}}'
+                elif data["meta"]["zebra_mode"] == "nb":
+                    int_str = f'{"{:8.2f}".format(correction(area, lorentz, data["meta"]["zebra_mode"], data["Measurements"][str(keys)]["gamma_angle"],data["Measurements"][str(keys)]["nu_angle"])):{align}{padding[2]}}'
+                    angle_str1 = f'{data["Measurements"][str(keys)]["gamma_angle"]:{padding[3]}}'
+                    angle_str2 = f'{data["Measurements"][str(keys)]["omega_angle"]:{padding[3]}}'
+                    angle_str3 = f'{data["Measurements"][str(keys)]["nu_angle"]:{padding[3]}}'
+                    angle_str4 = f'{data["Measurements"][str(keys)]["unkwn_angle"]:{padding[3]}}'
 
-            if data["meta"]["zebra_mode"] == "bi":
-                int_str = f'{"{:8.2f}".format(correction(area, lorentz, data["meta"]["zebra_mode"], meas["twotheta_angle"])):{align}{padding[2]}}'
-                angle_str1 = f'{meas["twotheta_angle"]:{padding[3]}}'
-                angle_str2 = f'{meas["omega_angle"]:{padding[3]}}'
-                angle_str3 = f'{meas["chi_angle"]:{padding[3]}}'
-                angle_str4 = f'{meas["phi_angle"]:{padding[3]}}'
-            elif data["meta"]["zebra_mode"] == "nb":
-                int_str = f'{"{:8.2f}".format(correction(area, lorentz, data["meta"]["zebra_mode"], meas["gamma_angle"],meas["nu_angle"])):{align}{padding[2]}}'
-                angle_str1 = f'{meas["gamma_angle"]:{padding[3]}}'
-                angle_str2 = f'{meas["omega_angle"]:{padding[3]}}'
-                angle_str3 = f'{meas["nu_angle"]:{padding[3]}}'
-                angle_str4 = f'{meas["unkwn_angle"]:{padding[3]}}'
+                line = (
+                    meas_number_str
+                    + h_str
+                    + l_str
+                    + k_str
+                    + int_str
+                    + sigma_str
+                    + angle_str1
+                    + angle_str2
+                    + angle_str3
+                    + angle_str4
+                    + "\n"
+                )
+                out_file.write(line)
 
-            sigma_str = f'{"{:8.2f}".format(float(meas["fit"]["g_width"][0])):{align}{padding[2]}}'
-            line = (
-                meas_number_str
-                + h_str
-                + l_str
-                + k_str
-                + int_str
-                + sigma_str
-                + angle_str1
-                + angle_str2
-                + angle_str3
-                + angle_str4
-                + "\n"
-            )
-            out_file.write(line)
+            except KeyError:
+                print("Measurement skipped - no fit value for:", keys)
