4b3b14e4ace826bdf2935111fa0ea6561435417a
Ivan Usov <ivan.usov@psi.ch>
2020-12-10 16:46:48 +0100

    Disable adding and removing fit funcs in param study

--- pyzebra/app/panel_param_study.py
+++ pyzebra/app/panel_param_study.py
@@ -450,6 +450,7 @@ def create():
             ("Pseudo Voigt1", "pseudovoigt1"),
         ],
         default_size=145,
+        disabled=True,
     )
     fitparams_add_dropdown.on_click(fitparams_add_dropdown_callback)
 
@@ -484,7 +485,7 @@ def create():
 
             fitparams_select.value = []
 
-    fitparams_remove_button = Button(label="Remove fit function", default_size=145)
+    fitparams_remove_button = Button(label="Remove fit function", default_size=145, disabled=True)
     fitparams_remove_button.on_click(fitparams_remove_button_callback)
 
     def fitparams_factory(function):
