426bb167924cf4fc1bbcc5ba7f5e58137936c896
Ivan Usov <ivan.usov@psi.ch>
2021-03-03 14:56:38 +0100

    Cleanup

--- pyzebra/__init__.py
+++ pyzebra/__init__.py
@@ -1,6 +1,5 @@
 from pyzebra.anatric import *
-from pyzebra.ccl_io import export_1D, load_1D, parse_1D
-from pyzebra.fit2 import fitccl
+from pyzebra.ccl_io import *
 from pyzebra.h5 import *
 from pyzebra.xtal import *
 from pyzebra.ccl_process import *
