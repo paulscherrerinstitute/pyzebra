a70b4fae575903a713f4b25251b944d4dfa3c9b1
Ivan Usov <ivan.usov@psi.ch>
2021-03-03 16:55:49 +0100

    Disable fit model change in ccl_integrate

--- pyzebra/app/panel_ccl_integrate.py
+++ pyzebra/app/panel_ccl_integrate.py
@@ -331,6 +331,7 @@ def create():
             # ("Pseudo Voigt1", "pseudovoigt1"),
         ],
         default_size=145,
+        disabled=True,
     )
     fitparams_add_dropdown.on_click(fitparams_add_dropdown_callback)
 
@@ -365,7 +366,7 @@ def create():
 
             fitparams_select.value = []
 
-    fitparams_remove_button = Button(label="Remove fit function", default_size=145)
+    fitparams_remove_button = Button(label="Remove fit function", default_size=145, disabled=True)
     fitparams_remove_button.on_click(fitparams_remove_button_callback)
 
     def fitparams_factory(function):
